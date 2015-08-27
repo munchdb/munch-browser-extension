f675d23da6016d8295869a8a31edbd994d0cb1ba
Darian Moody <mail@djm.org.uk>
2015-08-28 00:09:24 +0100

    Remove logging code from render.
    As we now have the possibility of multiple requests per page,
    these stats no longer make sense. They may be brought back at
    a later date if needed.

--- src/render.js
+++ src/render.js
@@ -1,13 +1,7 @@
-import {log} from './log'
 import {slugify} from './utils/text'
 
 export function renderResults (lookupMap, xhrEvent) {
   let response = xhrEvent.currentTarget.response
-  let requestedCount = lookupMap.size
-  let returnedCount = response.length
-  let percentReturned = (returnedCount / requestedCount) * 100
-  let ratedCount = 0
-  let notRatedCount = 0
 
   // Handle results that were found.
   for (let i in response) {
@@ -16,17 +10,12 @@ export function renderResults (lookupMap, xhrEvent) {
     let domElement = lookupMap.get(eatery.site_id)
     renderFound(eatery, domElement)
     lookupMap.delete(eatery.site_id)
-    ratedCount++
   }
 
   // Handle results that were not found.
   for (let [slug, domElement] of lookupMap.entries()) {
     renderNotFound(slug, domElement)
-    notRatedCount++
   }
-  let percentRated = (ratedCount / requestedCount) * 100
-  log(`Lookup results => ${requestedCount} requested from API, ${returnedCount} returned => ${percentReturned.toFixed(2)}%`)
-  log(`Lookup results => ${ratedCount} rated, ${notRatedCount} not => ${percentRated.toFixed(2)}%`)
 }
 
 function renderFound (eatery, domElement) {
