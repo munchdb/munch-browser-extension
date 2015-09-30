811740e0227c796d21b068c6409f6c3c6a3cbc8d
Darian Moody <mail@djm.org.uk>
2015-09-30 23:44:17 +0100

    Add support for tooltips.

--- src/render.js
+++ src/render.js
@@ -19,11 +19,33 @@ export function renderResults (lookupMap, xhrEvent) {
 }
 
 function renderFound (eatery, domElement) {
-  let span = document.createElement('span')
-  let sluggedRating = slugify(eatery.fsa_rating)
-  span.innerHTML = `FSA Rating: ${eatery.fsa_rating}`
-  span.className = `munch-rating munch-rating-${sluggedRating}`
-  domElement.appendChild(span)
+  let rating = document.createElement('a')
+  let sluggedRating = slugify(eatery.fsa.rating)
+  let address = [
+    eatery.fsa.address_line_1,
+    eatery.fsa.address_line_2,
+    eatery.fsa.address_line_3,
+    eatery.fsa.address_line_4,
+    eatery.fsa.postcode
+  ].filter(Boolean).join(',<br>')
+
+  rating.innerHTML = `
+    <div class="munch-rating-hint-wrapper">
+      <div class="hint__content munch-rating-hint">
+        <h4 class="munch-rating-hint-heading">${eatery.fsa.business_name}</h4>
+        <p class="munch-rating-hint-rating">FSA Rating: ${eatery.fsa.rating}</p>
+        <p class="munch-rating-hint-authority">Authority: ${eatery.fsa.local_authority_name}</p>
+        <p class="munch-rating-hint-date">Rating Date: ${eatery.fsa.rating_date}</p>
+        <p class="munch-rating-hint-address">${address}</p>
+        <a class="munch-rating-hint-url" href="${eatery.fsa.url}" title="Food Standards Agency" target="_blank">Visit on food.gov.uk</a>
+        </p>
+      </div>
+    </div>
+    `
+  rating.className = `hint hint--html hint--bottom munch-rating munch-rating-${sluggedRating}`
+  rating.href = eatery.fsa.url
+  rating.target = '_blank'
+  domElement.appendChild(rating)
 }
 
 function renderNotFound (slug, domElement) {
